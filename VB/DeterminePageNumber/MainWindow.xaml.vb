5e3066667855573183da9cdfaca100f56cecffa1
DevExpressExampleBot <fakeEmail>
2024-09-12 14:52:54 +0400

    Source auto update [skip ci]

--- VB/DeterminePageNumber/MainWindow.xaml.vb
+++ VB/DeterminePageNumber/MainWindow.xaml.vb
@@ -1,22 +1,20 @@
-﻿Imports DevExpress.Xpf.PdfViewer
+Imports DevExpress.Xpf.PdfViewer
 Imports System.Windows
 Imports System.Windows.Input
 
 Namespace DeterminePageNumber
 
-    Partial Public Class MainWindow
+    Public Partial Class MainWindow
         Inherits Window
 
-
         Public Sub New()
-            InitializeComponent()
-            pdfViewer.OpenDocument("..\..\demo.pdf")
+            Me.InitializeComponent()
+            Me.pdfViewer.OpenDocument("..\..\demo.pdf")
         End Sub
 
         Private Sub pdfViewer_MouseDown(ByVal sender As Object, ByVal e As MouseButtonEventArgs)
-            Dim result As PdfHitTestResult = pdfViewer.HitTest(e.GetPosition(pdfViewer))
+            Dim result As PdfHitTestResult = Me.pdfViewer.HitTest(e.GetPosition(Me.pdfViewer))
             MessageBox.Show(String.Format("You clicked on page {0}", result.DocumentPosition.PageNumber))
         End Sub
     End Class
 End Namespace
-
